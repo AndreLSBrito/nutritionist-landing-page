280a9a968d1a93621c8c844a9597dbc7a60aa0e5
Tiotedd <andrelsbrito2022@gmail.com>
2023-10-31 00:17:21 -0300

    feat: add buttons bottom to redirect image

--- src/app/components/carrossel.tsx
+++ src/app/components/carrossel.tsx
@@ -28,29 +28,53 @@ export function Carrossel({srcImages, width}: CarrosselProps){
   }
   
   return(
-    <div className='flex items-center'>
-      <div className='flex  justify-between items-center text-5xl text-primary'>
-        <button onClick={previousImage}>
-          <MdKeyboardArrowLeft size={24}/>
-        </button>
-      </div>
-
-      <div ref={carrossel} className={`${width} h-full flex transition overflow-x-auto ease-out duration-500 [&::-webkit-scrollbar]:hidden md:w-[550px] lg:w-full`}>
-        <div className="flex gap-1 [&::-webkit-scrollbar]:hidden" >
-
-        {srcImages.map((src, index) => (
-          <Image key={index} ref={image} src={src} alt="Print de conversa de WhatsApp" width={300} height={500} style={{transform: `translateX(-${currentImage * 102}%)`, }}/>
-        ))}
-      
-        </div>
-      </div>
-
-      <div className='flex  justify-between items-center text-5xl text-primary'>
-          <button onClick={nextImage}>
-            <MdKeyboardArrowRight size={24}/>
+    <div>
+      <div className='flex items-center'>
+        <div className='flex  justify-between items-center text-5xl text-primary'>
+          <button onClick={previousImage}>
+            <MdKeyboardArrowLeft size={24}/>
           </button>
+        </div>
+
+        <div ref={carrossel} className={`${width} flex transition overflow-x-auto ease-out duration-500 [&::-webkit-scrollbar]:hidden md:w-[550px] lg:w-full lg:h-[320px]`}>
+          <div className="flex h-full gap-1 [&::-webkit-scrollbar]:hidden" >
+
+          {srcImages.map((src, index) => (
+            <Image 
+              key={index} 
+              ref={image} 
+              src={src} 
+              alt="imagem em carrossel" 
+              width={300} 
+              height={500} 
+              style={{transform: `translateX(-${currentImage * 102}%)`, }}/>
+          ))}
+        
+          </div>
+        </div>
+
+        <div className='flex  justify-between items-center text-5xl text-primary'>
+            <button onClick={nextImage}>
+              <MdKeyboardArrowRight size={24}/>
+            </button>
+        </div>  
       </div>
       
+      <div 
+        
+        className='py-4 flex justify-center gap-3 w-full'
+      >
+        {srcImages.map((src, index) => (
+          <div 
+            key={"circle" + index}
+            onClick={() => {
+              setCurrentImage(index)
+            }}
+            className={`rounded-full w-3 h-3 cursor-pointer ${index == currentImage ? "bg-primary border border-white" : "bg-primary opacity-40 "}`}
+          ></div>
+        ))}
+      </div>
+
     </div>
   )
 }
