f6dc621231391ead0a43f8fde8a360d9f875f8ca
Tiotedd <andrelsbrito2022@gmail.com>
2023-11-12 13:55:05 -0300

    feat: add google analytics data flow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Footer } from './components/footer'
+import Script from 'next/script'
+import { GoogleAnalytics } from '@/components/googleAnalytics'
 
 const inter = Inter({ 
   weight: ['400', '700'],
@@ -11,8 +13,9 @@ const inter = Inter({
 
 
 export const metadata: Metadata = {
-  title: 'Fred Oliveira',
-  description: 'Nutricionista esportivo Fred Oliveira, para acompanhamento nutricional',
+  title: 'Fred Oliveira Nutricionista esportivo',
+  description: 'Nutricionista esportivo Fred Oliveira, para acompanhamento nutricional em Arcos, Minas Gerais',
+  keywords: ["Nutricionista", "Nutricionista Esportivo", "Ganho de massa muscular", "Dieta", "Nutricionista em Arcos"]
 }
 
 export default function RootLayout({
@@ -22,6 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="PT-BR">
+      <GoogleAnalytics GA_MEASUREMENT_ID='G-TXNVVNL5LP'/>
       <body className={inter.className}>
         {children}
         <Footer/>
@@ -29,3 +33,4 @@ export default function RootLayout({
     </html>
   )
 }
+<meta  />
