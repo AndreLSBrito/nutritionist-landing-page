79fce91e07cf3b5a0b199853dc32ef9ee9fb308a
André Luís <110799629+AndreLSBrito@users.noreply.github.com>
2024-07-05 18:39:03 +0000

    link para whatsapp do fred

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 export function Button({width =250, height=90}: ButtonProps){
   return(
     <button>
-      <Link href="https://linkedin.com/andrelsbrito">
+      <Link href="https://wa.me/553791981812?text=Ol%C3%A1,%20Fred%20quero%20fazer%20parte%20do%20projeto%2030%20dias">
         <Image src="/button.svg" alt="Quero começar!" width={width} height={height}/>
       </Link>
     </button>
